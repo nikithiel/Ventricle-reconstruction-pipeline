ae726ab496fa94b245e91c315d5ebad496fe20e3
genojayadi <116380542+genojayadi@users.noreply.github.com>
2026-06-25 20:55:04 +0200

    Added feature to transform meshes using a basic BvH tree transformation (WIP)

--- addons/geometrical_heart_reconstruction.py
+++ addons/geometrical_heart_reconstruction.py
@@ -652,7 +652,7 @@ class MESH_OT_poisson(bpy.types.Operator):
         for object in bpy.context.selected_objects: create_poisson_from_object_pointcloud(context, object) # Repeat Poisson-surface-reconstruction algorithm for all selected objects.
         return{'FINISHED'}
 
-def create_poisson_from_object_pointcloud(context, obj): #TODO IMPLEMENTATION DURCHLESEN
+def create_poisson_from_object_pointcloud(context, obj): 
     """Create poisson surface reconstruction for a single point cloud"""
     point_data = np.asarray(obj.data.vertices) # Get point data of current object.
     # Initialize and fill entries of object vertices.
@@ -2078,6 +2078,9 @@ class PANEL_Dev_tools(bpy.types.Panel):
         row = layout.row(align=True)
         row.operator('heart.plot_stl', text = "Show", icon = 'AXIS_FRONT')
         row.operator('heart.save_plot_stl', text = 'Save', icon = 'DISK_DRIVE')
+        # Test function to do a topologically preserving transformation trick
+        row = layout.row(align=True)
+        row.operator('heart.test_transform', text = "TEST", icon = "AXIS_FRONT")
 
 #------
 
@@ -2354,8 +2357,7 @@ class MESH_OT_plot_STL(bpy.types.Operator):
 
         plt.show(block=True)
         
-        return{'FINISHED'}
-    
+        return{'FINISHED'}    
 class MESH_OT_save_plot_STL(bpy.types.Operator):
     """Save the plot without displaying and also any additional files within the input folder"""
     bl_idname = 'heart.save_plot_stl'
@@ -2392,6 +2394,7 @@ class MESH_OT_save_plot_STL(bpy.types.Operator):
         fig.savefig(plot_path)
         
         return{'FINISHED'}
+    
 # -------------------------------------------------------------------
 # Helpers for ventricle detection and STL export
 # -------------------------------------------------------------------
@@ -2431,6 +2434,7 @@ def find_ventricle_objects(objs):
     items.sort(key=lambda x: x[0])
     return [o for (_, o) in items]
 
+from mathutils.bvhtree import BVHTree
 def export_object_to_stl(obj, filepath, depsgraph=None):
     """Custom ASCII STL export for a single object."""
     if depsgraph is None:
@@ -2465,6 +2469,52 @@ def export_object_to_stl(obj, filepath, depsgraph=None):
 
     eval_obj.to_mesh_clear()
 
+class MESH_OT_test_transform(bpy.types.Operator):
+    """Reshape basal region for connetivity. Currently uses the object with max mesh count as reference (WIP)"""
+    bl_idname = 'heart.test_transform'
+    bl_label = 'Test Transformation'
+    def execute(self,context):
+        scene = context.scene
+        view_layer = context.view_layer
+
+        # --- Remember original selection & active object, also which meshes were selected ---
+        selected_objects = context.selected_objects
+        
+        if len(selected_objects) == 0 or not selected_objects:
+            cons_print(f"No objects selected")
+            return False
+
+        max = 0
+        for a in selected_objects:
+            if len(bmesh.from_edit_mesh(a.data).verts) > max:
+                source = a
+                max = len(bmesh.from_edit_mesh(a.data).verts)
+        
+        cons_print(f"Object used as reference {source.name}")
+        
+        bpy.ops.object.mode_set(mode='OBJECT') 
+        
+        for obj in selected_objects:
+            if obj != source:
+                cons_print(f"current object target {obj.name}")
+                copied_source = copy_object(source.name, obj.name + "_transformed")
+                BvH_transform(copied_source,obj)
+                copied_source.data.update()
+        cons_print(f"Operation completed")    
+        return{"FINISHED"}
+
+def BvH_transform(source,target):
+    cons_print(f"source {source}")
+    cons_print(f"target {target}")
+    bvh = BVHTree.FromObject(target, bpy.context.evaluated_depsgraph_get())
+    
+    for v in source.data.vertices:
+        world_co = source.matrix_world @ v.co
+
+        loc, normal, index, dist = bvh.find_nearest(world_co)
+
+        if loc:
+            v.co = source.matrix_world.inverted() @ loc
     
 classes = [
     PANEL_Files, MESH_OT_export_ventricle, MESH_OT_import_ventricle, PANEL_Position_Ventricle, MESH_OT_quick_reset, MESH_OT_ApproachSelection,
@@ -2473,7 +2523,7 @@ classes = [
     MESH_OT_create_basal, MESH_OT_connect_apical_and_basal, MESH_OT_Ventricle_Interpolation, MESH_OT_Add_Vessels_Valves, MESH_OT_check_node_connectivity,
 ]
 
-dev_classes = [PANEL_Poisson, MESH_OT_poisson, MESH_OT_create_valve_orifice, MESH_OT_connect_valves, PANEL_Dev_tools, MESH_DEV_volumes, MESH_DEV_indices, MESH_DEV_edge_index, MESH_DEV_color_min_dist, MESH_DEV_test, MESH_OT_plot_STL, MESH_OT_save_plot_STL]
+dev_classes = [PANEL_Poisson, MESH_OT_poisson, MESH_OT_create_valve_orifice, MESH_OT_connect_valves, PANEL_Dev_tools, MESH_DEV_volumes, MESH_DEV_indices, MESH_DEV_edge_index, MESH_DEV_color_min_dist, MESH_DEV_test, MESH_OT_plot_STL, MESH_OT_save_plot_STL, MESH_OT_test_transform]
   
 def register():
     # Position variables.
